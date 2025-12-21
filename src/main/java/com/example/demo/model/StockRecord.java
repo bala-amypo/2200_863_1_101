15f0181ed15fb04958f6aefca3425cc4fbec63c0
bala-amypo <bala@amypo.in>
2025-12-21 17:39:27 +0000

    User commit

---
+++ src/main/java/com/example/demo/model/StockRecord.java
@@ -0,0 +1,32 @@
+package com.example.demo.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(
+    name = "stock_records",
+    uniqueConstraints = @UniqueConstraint(columnNames = {"product_id", "warehouse_id"})
+)
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class StockRecord {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private Product product;
+
+    @ManyToOne
+    private Warehouse warehouse;
+
+    private Integer currentQuantity;
+    private Integer reorderThreshold;
+    private LocalDateTime lastUpdated;
+}

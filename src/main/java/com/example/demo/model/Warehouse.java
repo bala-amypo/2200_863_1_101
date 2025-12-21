27d20bd0c0f3ab79a35434f86f5f85a2f7d66ea7
bala-amypo <bala@amypo.in>
2025-12-21 18:15:28 +0000

    User commit

--- src/main/java/com/example/demo/model/Warehouse.java
+++ src/main/java/com/example/demo/model/Warehouse.java
@@ -1,25 +1,13 @@
 package com.example.demo.model;
 
-import jakarta.persistence.*;
-import lombok.*;
-
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
-@Entity
-public class Warehouse {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    private String name;
-}
-package com.example.demo.model;
-
-import jakarta.persistence.*;
-import lombok.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor

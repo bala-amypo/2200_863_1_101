150af5458d4c7bdfdae0413bb9a2d6057a5c5e2a
bala-amypo <bala@amypo.in>
2025-12-22 10:02:50 +0000

    User commit

--- src/main/java/com/example/demo/controller/ProductController.java
+++ src/main/java/com/example/demo/controller/ProductController.java
@@ -2,21 +2,23 @@ package com.example.demo.controller;
 
 import com.example.demo.model.Product;
 import com.example.demo.service.ProductService;
-import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/products")
-@RequiredArgsConstructor
 public class ProductController {
 
     private final ProductService service;
 
+    public ProductController(ProductService service) {
+        this.service = service;
+    }
+
     @PostMapping
     public Product create(@RequestBody Product product) {
-        return service.save(product);
+        return service.create(product);
     }
 
     @GetMapping
